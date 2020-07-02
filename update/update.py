27fc783e59c740192fa251e5d9473ecd547ec913
Mauricio Foronda <josemauricioforonda@gmail.com>
2020-07-02 14:35:56 -0400

    incluyo datos para casos con zona no identificada

--- update/update.py
+++ update/update.py
@@ -4,31 +4,46 @@ import pandas as pd
 import pytz
 from  datetime import datetime, timedelta
 
+def get_total():
+  url = 'http://observatoriocovid19.lapaz.bo/obsadmin/casosAcumulados.php?accion=json'
+  response = requests.get(url).json()
+  r = [int(response[i]) for i in ['confirmadosMunicipio', 'recuperadosMunicipio', 'fallecidosMunicipio']]
+  return {'confirmados': r[0],
+          'activos': r[0] - r[1] - r[2],
+          'recuperados': r[1],
+          'total': r[]
+          'fallecidos': r[2]}
+
 def get_data():
   url = 'http://observatoriocovid19.lapaz.bo/obsadmin/casosXZona.php'
   data = pd.DataFrame(requests.get(url).json()['data'])
   data.index = (data['macro'] + '.' + data['distrito']).astype(float)
   data = data.sort_index()
-  data[['distrito', 'macro', 'positivo', 'recuperado',
-       'fallecido', 'total']] = data[['distrito', 'macro', 'positivo', 'recuperado','fallecido', 'total']].astype(int)
-  return data
+  data[['distrito', 'macro', 'positivo', 'recuperado', 'fallecido', 'total']] = data[['distrito', 'macro', 'positivo', 'recuperado','fallecido', 'total']].astype(int)
+  cols = ['zona', 'distrito', 'macro', 'macrodistrito', 'activos', 'recuperados', 'fallecidos', 'confirmados']
+  data.columns = cols
+  total = get_total()
+  ni = ['No Identificado', 0, 0, 'NO IDENTIFICADO'] + [total[i] - data[i].sum() for i in ['activos', 'recuperados', 'fallecidos', 'confirmados']]
+  ni = {cols[n]:i for n, i in enumerate(ni)}
+  return data.append(pd.DataFrame([ni]))
 
 def get_saved():
-  return [pd.read_csv('{}.csv'.format(col)) for col in ['positivo', 'recuperado', 'fallecido']]
+  return [pd.read_csv('{}.csv'.format(col)) for col in ['confirmados', 'activos', 'recuperados', 'fallecidos']]
 
 def day_is():
   return datetime.strftime(datetime.now(pytz.timezone('America/La_Paz')) - timedelta(days=1), '%Y-%m-%d')
 
 def save_data(data, day):
-  for col in ['positivo', 'recuperado', 'fallecido']:
+  for col in ['confirmados', 'activos', 'recuperados', 'fallecidos']:
     data_col = data[['macrodistrito', 'macro', 'distrito', 'zona', col]]
     data_col = data_col.rename(columns={col:day})
     data_col = data_col[['macrodistrito', 'macro', 'distrito', 'zona', day]]
     data_col.to_csv('{}.csv'.format(col), index=False)
     
-def save_newday(day, data, positivo, recuperado, fallecido):
+def save_newday(day, data, saved):
+  confirmados, activos, recuperados, fallecidos = saved
   data = data.set_index('zona', drop=False)
-  for col in [['positivo', positivo], ['recuperado', recuperado], ['fallecido', fallecido]]:
+  for col in [['confirmados', confirmados], ['activos', activos], ['recuperados', recuperados], ['fallecidos', fallecidos]]:
     data_day = data[['zona', 'distrito', 'macro', 'macrodistrito', col[0]]]
     data_day = data_day.rename(columns={col[0]:day})
     data_col = col[1].set_index('zona', drop=False)
@@ -38,10 +53,9 @@ def save_newday(day, data, positivo, recuperado, fallecido):
     data_final = data_final.fillna(0)
     data_final[data_final.columns[4:]] = data_final[data_final.columns[4:]].astype(int)
     data_final.fillna(0).to_csv('{}.csv'.format(col[0]), index=False)
-    print(day)
+  print(day)
   
 data = get_data()
-positivo, recuperado, fallecido = get_saved()
 day = day_is()
 # save_data(d, day)
-save_newday(day, data, positivo, recuperado, fallecido)
+save_newday(day, data, get_saved())
